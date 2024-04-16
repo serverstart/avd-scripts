f2cabcf7507e9de0a90af0016f15229b16066b8e
Thomas <thomas@vdw-aachen.de>
2024-04-16 15:59:51 +0000

    wip

--- InstallMicrosoftTeams.ps1
+++ InstallMicrosoftTeams.ps1
@@ -11,21 +11,6 @@ New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Appx" -Name "A
 $MachineWide = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Teams Machine-Wide Installer"}
 $MachineWide.Uninstall()
 
-# Remove Teams for Current Users
-$localAppData = "$($env:LOCALAPPDATA)\Microsoft\Teams"
-$programData = "$($env:ProgramData)\$($env:USERNAME)\Microsoft\Teams"
-If (Test-Path "$($localAppData)\Current\Teams.exe") 
-{
-  unInstallTeams($localAppData)
-    
-}
-elseif (Test-Path "$($programData)\Current\Teams.exe") {
-  unInstallTeams($programData)
-}
-else {
-  Write-Warning  "Teams installation not found"
-}
-
 # Download and install WebView2
 (New-Object System.Net.WebClient).DownloadFile("https://go.microsoft.com/fwlink/p/?LinkId=2124703", "$($env:temp)\WebView2.exe")
 Start-Process -FilePath "$($env:temp)\WebView2.exe" -Wait -ArgumentList "/silent /install" -ErrorAction SilentlyContinue
@@ -34,19 +19,3 @@ Start-Process -FilePath "$($env:temp)\WebView2.exe" -Wait -ArgumentList "/silent
 (New-Object System.Net.WebClient).DownloadFile("https://go.microsoft.com/fwlink/?linkid=2243204", "$($env:temp)\TeamsBootstrapper.exe")
 $rv=Start-Process -FilePath "$($env:temp)\TeamsBootstrapper.exe" -Wait -ArgumentList "-p" -PassThru -ErrorAction SilentlyContinue
 $rv.ExitCode
-
-# Function to Uninstall Teams on given path
-function unInstallTeams($path) {
-  $clientInstaller = "$($path)\Update.exe"
-  
-   try {
-        $process = Start-Process -FilePath "$clientInstaller" -ArgumentList "--uninstall /s" -PassThru -Wait -ErrorAction STOP
-        if ($process.ExitCode -ne 0)
-    {
-      Write-Error "UnInstallation failed with exit code  $($process.ExitCode)."
-        }
-    }
-    catch {
-        Write-Error $_.Exception.Message
-    }
-}
