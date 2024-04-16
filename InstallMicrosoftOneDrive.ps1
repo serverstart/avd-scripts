dbff3d8664d8c5199c073044da2743aa1a8a44b5
Thomas <thomas@vdw-aachen.de>
2024-04-16 16:23:21 +0000

    wip

--- InstallMicrosoftOneDrive.ps1
+++ InstallMicrosoftOneDrive.ps1
@@ -1,11 +1,5 @@
 # Vorlage: https://github.com/MicrosoftDocs/azure-docs/blob/main/articles/virtual-desktop/install-office-on-wvd-master-image.md
 
-# Azure Tenant ID
-param (
-    [string]$AzureAdTenantId = ""
-)
-
-
 # Download OneDrive Setup
 (New-Object System.Net.WebClient).DownloadFile("https://go.microsoft.com/fwlink/p/?LinkId=844652", "$($env:temp)\OneDriveSetup.exe")
 
@@ -17,7 +11,8 @@ New-Item -Path "HKLM:\SOFTWARE\Microsoft" -Name "OneDrive" -Force -ErrorAction I
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\OneDrive" -Name "AllUsersInstall" -PropertyType DWord -Value 1 -force
 
 # Install OneDrive for all users
-Start-Process -FilePath "$($env:temp)\OneDriveSetup.exe" -Wait -ArgumentList "/silent /allusers" -ErrorAction SilentlyContinue
+Start-Process -FilePath "$($env:temp)\OneDriveSetup.exe" -ArgumentList "/silent /allusers" -ErrorAction SilentlyContinue
+Wait-Event -Timeout 5
 
 # Configure OneDrive to start at sign in for all users
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "OneDrive" -PropertyType String -Value "C:\Program Files\Microsoft OneDrive\OneDrive.exe /background" -force
@@ -25,8 +20,3 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Na
 # Enable Silently configure user account by running the following command.
 New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft" -Name "OneDrive" -Force -ErrorAction Ignore
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive" -Name "SilentAccountConfig" -PropertyType DWord -Value 1 -force
-
-#Redirect and move Windows known folders (Desktop, Documents, etc.) to OneDrive by running the following command.
-# if ($AzureAdTenantId -ne "") {
-#     New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive" -Name "KFMSilentOptIn" -PropertyType String -Value $AzureAdTenantId -force
-# }
